7df930ad9b21d1b70d5d111ce5abcbb9ea7e54a2
Farzin Khaleghian <fkhaleghian@gmail.com>
2021-02-04 10:25:03 -0600

    add feed router to express

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 
+const feedRoutes = require('./routes/feed');
+
 const app = express();
+
+app.use('./post', feedRoutes);
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
